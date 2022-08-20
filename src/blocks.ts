be3f80f2308db4f6398808bac7e36619d54145ba
Vladimir <azuresplash@gmail.com>
2022-08-20 09:09:16 +0200

    Added simplex noise

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,11 +1,13 @@
 import * as THREE from "three";
+import { createNoise2D } from "simplex-noise";
+
 let camera, scene, renderer;
 let mesh;
 
 const cubesize = 30;
-const landscape_width = 30;
-const landscape_length = 30;
-const heights = [];
+const landscape_width = 70;
+const landscape_length = 70;
+const heights: number[][] = [];
 const camera_offset = cubesize * landscape_width * 0.7;
 const camera_height = (cubesize * landscape_width) / 2;
 
@@ -22,36 +24,22 @@ function init() {
 
   const xoff = (landscape_width * cubesize) / 2;
   const yoff = (landscape_length * cubesize) / 2;
-  for (let i = 0; i < landscape_width; i++) heights[(i, 0)] = 0;
-  for (let j = 0; j < landscape_length; j++) heights[(0, j)] = 0;
-
+  const noise2D = createNoise2D();
+  const noise_scale = 0.05;
   for (let i = 1; i < landscape_width; i++) {
-    const h = heights[(i - 1, 0)];
+    heights[i] = [];
     for (let j = 1; j < landscape_length; j++) {
-      const rand = Math.random();
-      if (heights[(i - 1, j)] == heights[(i, j - 1)]) {
-        // level ground, go up dn or stay
-        if (rand < 0.33) heights[(i, j)] = heights[(i - 1, j)] - cubesize;
-        else if (rand > 0.66) heights[(i, j)] = heights[(i - 1, j)] + cubesize;
-        else heights[(i, j)] = heights[(i - 1, j)];
-      } else if (Math.abs(heights[(i - 1, j)] - heights[(i, j - 1)]) > cubesize) {
-        // two edges are wide apart, split the difference
-        heights[(i, j)] = (heights[(i - 1, j)] + heights[(i, j - 1)]) / 2;
-      } else {
-        if (rand > 0.5) heights[(i, j)] = heights[(i - 1, j)];
-        else heights[(i, j)] = heights[(i, j - 1)];
-      }
-
-      const grayness = Math.random() * 0.5 + 0.25,
+      heights[i][j] = Math.floor(noise2D(noise_scale * i, noise_scale * j) * 10);
+      console.log(heights);
+      const grayness = 1 - noise2D(noise_scale * i, noise_scale * j),
         mat = new THREE.MeshBasicMaterial(),
         cube = new THREE.Mesh(geom, mat);
       mat.color.setRGB(0, grayness, 0);
-      console.log(heights);
-      cube.position.set(i * cubesize - xoff, j * cubesize - yoff, heights[(i, j)]);
+      cube.position.set(i * cubesize - xoff, j * cubesize - yoff, heights[i][j] * 30);
       cubes.add(cube);
     }
   }
-
+  console.log(heights);
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -64,9 +52,9 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-const angle = 0;
+let angle = 0;
 function animate() {
-  //angle += 0.2;
+  angle += 0.2;
   const rads = (angle * Math.PI) / 180;
   camera.position.set(Math.cos(rads) * camera_offset, Math.sin(rads) * camera_offset, camera_height);
   camera.lookAt(scene.position);
@@ -77,3 +65,15 @@ function animate() {
 init();
 
 animate();
+
+///
+
+type BiomeType = "white" | "green" | "brown";
+
+type ElevationMap = number[][];
+type TerrainBlock = {
+  position: [number, number, number];
+  elevation: number;
+  biomeType: BiomeType;
+};
+function renderElevationMap(map: ElevationMap) {}
