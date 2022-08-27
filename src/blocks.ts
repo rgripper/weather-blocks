53d23ae017fb73bc915011ae9fee9ea74da9d407
Vladimir <azuresplash@gmail.com>
2022-08-27 15:47:17 +0200

    Added water

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,31 +1,42 @@
 import * as THREE from "three";
 import { createNoise2D } from "simplex-noise";
 
-let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer;
+let camera: THREE.PerspectiveCamera;
+let scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
 
 const cubesize = 30;
 const landscape_width = 70;
 const landscape_length = 70;
-const heights: number[][] = [];
 const camera_offset = cubesize * landscape_width * 0.7;
 const camera_height = (cubesize * landscape_width) / 2;
 
 function init() {
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
   camera.position.set(0, camera_offset, camera_height);
   camera.up = new THREE.Vector3(0, 0, 1);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
   scene = new THREE.Scene();
 
   const geom = new THREE.BoxGeometry(cubesize, cubesize, cubesize);
-  const cubes = new THREE.Object3D();
+
+  const items = Array.from(generateElevationMap()).map((block) => ({
+    mesh: convertTerrainBlockToMesh(block, geom),
+    block,
+  }));
+
+  const cubes = items.reduce(
+    (cubes, item) => cubes.add(item.mesh),
+    new THREE.Object3D()
+  );
+
   scene.add(cubes);
 
-  Array.from(generateElevationMap())
-    .map((x) => convertTerrainBlockToGeometry(x, geom))
-    .forEach((x) => cubes.add(x));
-
-  console.log(heights);
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -42,7 +53,11 @@ let angle = 0;
 function animate() {
   angle += 0.2;
   const rads = (angle * Math.PI) / 180;
-  camera.position.set(Math.cos(rads) * camera_offset, Math.sin(rads) * camera_offset, camera_height);
+  camera.position.set(
+    Math.cos(rads) * camera_offset,
+    Math.sin(rads) * camera_offset,
+    camera_height
+  );
   camera.lookAt(scene.position);
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
@@ -52,7 +67,7 @@ init();
 
 animate();
 
-type BiomeType = "white" | "green" | "brown";
+type BiomeType = "white" | "green" | "brown" | "water";
 
 type TerrainBlock = {
   position: [number, number, number];
@@ -65,23 +80,39 @@ function* generateElevationMap() {
   const noise_scale = 0.02;
   for (let i = 1; i < landscape_width; i++) {
     for (let j = 1; j < landscape_length; j++) {
-      const elevation = Math.floor(noise2D(noise_scale * i, noise_scale * j) * 10);
+      const elevation = Math.floor(
+        noise2D(noise_scale * i, noise_scale * j) * 10
+      );
       const block: TerrainBlock = {
         position: [i - xoff, j - yoff, elevation],
-        biomeType: elevation > 6 ? "white" : elevation > 4 ? "brown" : "green",
+        biomeType:
+          elevation > 6
+            ? "white"
+            : elevation > 4
+            ? "brown"
+            : elevation < 0
+            ? "water"
+            : "green",
       };
       yield block;
     }
   }
 }
 
-function convertTerrainBlockToGeometry(
+function convertTerrainBlockToMesh(
   { position: [x, y, z], biomeType }: TerrainBlock,
   boxGeometry: THREE.BoxGeometry
 ) {
   const mat = new THREE.MeshBasicMaterial();
   const cube = new THREE.Mesh(boxGeometry, mat);
-  const color = biomeType === "brown" ? 0x84694e : biomeType === "green" ? 0x7cfc00 : 0xeeeeee;
+  const color =
+    biomeType === "brown"
+      ? 0x84694e
+      : biomeType === "green"
+      ? 0x7cfc00
+      : biomeType === "water"
+      ? 0x33aaff
+      : 0xeeeeee;
 
   function hexWithGray(hex: number, factor: number) {
     var r = ((hex >> 16) & 255) * factor;
@@ -95,8 +126,12 @@ function convertTerrainBlockToGeometry(
 
   mat.color.setHex(hexWithGray(color, grayFactor));
 
-  console.log(grayFactor, color.toString(16), (color % (256 * 256)).toString(16));
+  console.log(
+    grayFactor,
+    color.toString(16),
+    (color % (256 * 256)).toString(16)
+  );
 
-  cube.position.set(x * cubesize, y * cubesize, z * cubesize);
+  cube.position.set(x * cubesize, y * cubesize, Math.max(z, 0) * cubesize);
   return cube;
 }
