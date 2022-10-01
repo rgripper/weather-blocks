facac854bf88483688fc9d3e4be4ce2caa302099
Vladimir <azuresplash@gmail.com>
2022-10-01 18:34:05 +0200

    Added moon light

--- src/TerrainPreview.tsx
+++ src/TerrainPreview.tsx
@@ -32,14 +32,14 @@ function init({
 
   const geom = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
 
-  const light = new THREE.DirectionalLight(0xffffff, 1.5);
-  light.position.set(40, 60, 225);
-  light.shadow.mapSize.width = 512; // default
-  light.shadow.mapSize.height = 512; // default
-  light.castShadow = true;
-  light.shadow.bias = -0.005; // reduces self-shadowing on double-sided objects
+  const sunLight = new THREE.DirectionalLight(0xffffff, 1);
+  sunLight.position.set(40, 60, 225);
+  sunLight.shadow.mapSize.width = 512; // default
+  sunLight.shadow.mapSize.height = 512; // default
+  sunLight.castShadow = true;
+  sunLight.shadow.bias = -0.005; // reduces self-shadowing on double-sided objects
 
-  light.add(
+  sunLight.add(
     new THREE.Mesh(
       new THREE.SphereGeometry(50, 10, 10),
       new THREE.MeshBasicMaterial({
@@ -47,6 +47,24 @@ function init({
       })
     )
   );
+
+  const defaultMoonlightIntencity = 0.02
+  const moonLight = new THREE.DirectionalLight(0xefffff, defaultMoonlightIntencity);
+  moonLight.position.set(140, 160, 225);
+  moonLight.shadow.mapSize.width = 512; // default
+  moonLight.shadow.mapSize.height = 512; // default
+  moonLight.castShadow = true;
+
+  moonLight.add(
+    new THREE.Mesh(
+      new THREE.SphereGeometry(50, 10, 10),
+      new THREE.MeshBasicMaterial({
+        color: 0xeeffff,
+      })
+    )
+  );
+
+  moonLight.shadow.bias = -0.005; // reduces self-shadowing on double-sided objects
 
   const items = Array.from(elevationBlocks).map((block) => ({
     mesh: convertTerrainBlockToMesh(block, geom, cubeSize),
@@ -59,7 +77,8 @@ function init({
   );
 
   scene.add(cubes);
-  scene.add(light);
+  scene.add(sunLight);
+  scene.add(moonLight);
 
   const renderer = new THREE.WebGLRenderer();
   renderer.shadowMap.enabled = true;
@@ -82,7 +101,9 @@ function init({
     renderer: THREE.Renderer,
     scene: THREE.Scene,
     angle: number,
-    sunPosition: SunCalc.GetSunPositionResult
+    sunPosition: SunCalc.GetSunPositionResult,
+    moonPosition: SunCalc.GetMoonPositionResult,
+    moonIllumination: SunCalc.GetMoonIlluminationResult
   ) {
     const rads = (angle * Math.PI) / 180;
     camera.position.set(
@@ -91,53 +112,84 @@ function init({
       cameraHeight
     );
 
-    function componentToHex(c: number): string {
-      var hex = c.toString(16);
-      return hex.length == 1 ? "0" + hex : hex;
-    }
-
-    function hexToRgb(hex: number) {
-      var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
-        hex.toString(16).padStart(6, "0")
-      )!;
-      return {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      };
-    }
-
-    function rgbToHex({ r, g, b }: { r: number; g: number; b: number }) {
-      return parseInt(
-        componentToHex(r) + componentToHex(g) + componentToHex(b),
-        16
-      );
-    }
-
-    const fastGrowthAndFlatMid = (x: number) => Math.log10(1 + x * 9)
+    const fastGrowthAndFlatMid = (x: number) => Math.log10(1 + x * 9);
     const preceivedIntensity = fastGrowthAndFlatMid(sunPosition.insolation);
     const peakColor = 0xffffff;
-    const rgb = hexToRgb(peakColor);
-    rgb.b = Math.round(rgb.b * (1 + sunPosition.altitude) / 2);
-    rgb.g = Math.round(rgb.g * (1 + sunPosition.altitude) / 2);
+    const altitudeFilteredSunlightColor = adjustLightByAltitude(
+      peakColor,
+      sunPosition
+    );
 
-    const altitudeFilteredColor = rgbToHex(rgb);
-    console.log(rgb, altitudeFilteredColor.toString(16).padStart(6, "0"));
+    const altitudeFilteredMoonlightColor = adjustLightByAltitude(
+      peakColor,
+      moonPosition
+    );
 
-    light.intensity = preceivedIntensity;
-    light.color = new THREE.Color(altitudeFilteredColor);
-    light.lookAt(scene.position);
+    sunLight.intensity = preceivedIntensity;
+    sunLight.color = new THREE.Color(altitudeFilteredSunlightColor);
+    sunLight.lookAt(scene.position);
+
+    console.log(defaultMoonlightIntencity * moonPosition.altitude * moonIllumination.fraction)
+    moonLight.intensity = defaultMoonlightIntencity * moonPosition.altitude * moonIllumination.fraction;
+    moonLight.color = new THREE.Color(altitudeFilteredMoonlightColor);
+    moonLight.lookAt(scene.position);
+
     camera.lookAt(scene.position);
 
     renderer.render(scene, camera);
   }
 
   return {
-    render: (angle: number, sunPosition: SunCalc.GetSunPositionResult) => {
-      render(camera, renderer, scene, angle, sunPosition);
+    render: (
+      angle: number,
+      sunPosition: SunCalc.GetSunPositionResult,
+      moonPosition: SunCalc.GetMoonPositionResult,
+      moonIllumination: SunCalc.GetMoonIlluminationResult
+    ) => {
+      render(camera, renderer, scene, angle, sunPosition, moonPosition, moonIllumination);
     },
     element: renderer.domElement,
   };
+}
+
+function adjustLightByAltitude(
+  peakColor: number,
+  position: SunCalc.GetSunPositionResult
+) {
+  function componentToHex(c: number): string {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  function hexToRgb(hex: number) {
+    var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+      hex.toString(16).padStart(6, "0")
+    )!;
+    return {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    };
+  }
+
+  function rgbToHex({ r, g, b }: { r: number; g: number; b: number }) {
+    return parseInt(
+      componentToHex(r) + componentToHex(g) + componentToHex(b),
+      16
+    );
+  }
+
+  const rgb = hexToRgb(peakColor);
+  rgb.b = Math.round((rgb.b * (1 + position.altitude / (Math.PI / 2))) / 2);
+  rgb.g = Math.round((rgb.g * (1 + position.altitude / (Math.PI / 2))) / 2);
+  const altitudeFilteredColor = rgbToHex(rgb);
+  console.log(
+    position.altitude,
+    (1 + position.altitude) / 2,
+    peakColor.toString(16),
+    altitudeFilteredColor.toString(16)
+  );
+  return altitudeFilteredColor;
 }
 
 export function TerrainPreview({
@@ -160,7 +212,7 @@ export function TerrainPreview({
 
       let angle = 0;
       let isDone = false;
-      const date = new Date(2022, 8, 24);
+      const date = new Date(2022, 7, 24);
       const tick = () => {
         if (isDone) return;
 
@@ -217,23 +269,30 @@ export function TerrainPreview({
           );
         }
 
-        const position = SunCalc.getPosition(date, 40.1789, -3.5156);
+        const sunPosition = SunCalc.getPosition(date, 40.1789, -3.5156);
         console.log(
           date.getHours(),
           hourInsolaton(40.1789, date.getHours(), daysIntoYear(date)).toFixed(
             2
           ),
-          position.altitude.toFixed(2)
+          sunPosition.altitude.toFixed(2)
         );
-        ///
-        render(angle, {
-          ...position,
-          insolation: hourInsolaton(
-            40.1789,
-            date.getHours() + date.getMinutes() / 60,
-            daysIntoYear(date)
-          ),
-        });
+        const moonPosition = SunCalc.getMoonPosition(date, 40.1789, -3.5156);
+        const moonIllumination = SunCalc.getMoonIllumination(date);
+
+        render(
+          angle,
+          {
+            ...sunPosition,
+            insolation: hourInsolaton(
+              40.1789,
+              date.getHours() + date.getMinutes() / 60,
+              daysIntoYear(date)
+            ),
+          },
+          moonPosition,
+          moonIllumination
+        );
         date.setTime(date.getTime() + 1000 * 60 * 2);
         angle = angle > 360 ? 0 : angle + 0.2;
         window.requestAnimationFrame(tick);
